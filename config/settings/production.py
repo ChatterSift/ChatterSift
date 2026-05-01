851a1eee8297e052517d17165416de3c3ec4f6cc
Walden Cui <yingn.cui@gmail.com>
2026-05-01 00:36:28 +0000

    Change to psycopg[binary] and add missing AWS_S3_ENDPOINT_URL

--- config/settings/production.py
+++ config/settings/production.py
@@ -88,6 +88,7 @@ if USE_S3:
     )
     AWS_S3_REGION_NAME = env("DJANGO_AWS_S3_REGION_NAME", default=None)
     AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
+    AWS_S3_ENDPOINT_URL = env("DJANGO_AWS_S3_ENDPOINT_URL", default=None)
     aws_s3_domain = (
         AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
     )
